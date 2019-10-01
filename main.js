c17321fee902d2e14a6c51b20d4d8a496577dc98
Jesse Gould <jesse.gould@codeimmersives.com>
2019-10-01 15:19:52 -0400

    green lights

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
 function fizzy(num) {
-
+  let response = ' ';
+  const fizz = (num%3===0);
+  const buzz = (num%5===0);
+if (fizz && buzz){
+response ='FizzBuzz';
 }
+else if (fizz && !buzz) {
+response = 'Fizz';
+}
+else if (!fizz && buzz){
+  response = 'Buzz';
+}
+else if (!fizz && !buzz){
+  response = num;
+}
+return response;
+}
+
+
+console.log(fizzy(9));
+
 
 module.exports = {
   fizzy,
